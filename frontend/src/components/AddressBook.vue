66097ad050c1a01fe2808df7f8854825805dac4e
Ivan Belyavtsev <ibelyavtsev@compute-practice.ru>
2018-07-04 18:13:14 +0300

    Убрано все лишнее из проекта Задача #5

---
+++ frontend/src/components/AddressBook.vue
@@ -0,0 +1,19 @@
+<template>
+  <div class="addressbook">
+    <h1>{{ title }}</h1>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'AddressBook',
+  props: {
+    title: String
+  }
+}
+</script>
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style scoped>
+
+</style>
