7b597b342dd1b621d79df4676e962e03e27d93b6
Ivan Belyavtsev <ibelyavtsev@compute-practice.ru>
2018-07-04 16:48:40 +0300

    Создан пакет utils Задача #3

--- backend/main.go
+++ backend/main.go
@@ -1,12 +1,11 @@
 package main
 
-import "fmt"
+import (
+	"fmt"
 
-// GetHelloWorldString возвращает строку с приветствием
-func GetHelloWorldString(name string) string {
-	return fmt.Sprintf("Hello, %s", name)
-}
+	"github.com/ComputePractice2018/AddressBook/backend/utils"
+)
 
 func main() {
-	fmt.Println(GetHelloWorldString("Иван"))
+	fmt.Println(utils.GetHelloWorldString("Иван"))
 }
