a68473e0cfd59f64cfdd9cae30c0002a4b27cae4
Ivan Belyavtsev <ibelyavtsev@compute-practice.ru>
2018-07-06 18:54:09 +0300

    Сделан REST API Задача #16

--- backend/server/handles.go
+++ backend/server/handles.go
@@ -5,28 +5,22 @@ import (
 	"fmt"
 	"log"
 	"net/http"
+	"strconv"
 
 	"github.com/ComputePractice2018/AddressBook/backend/data"
+	"github.com/gorilla/mux"
 )
 
 //GetContacts обрабатывает запросы на получение списка контактов
 func GetContacts(w http.ResponseWriter, r *http.Request) {
-	binaryData, err := json.Marshal(data.ContactList)
+	w.Header().Add("Content-Type", "application/json; charset=utf-8")
+	w.WriteHeader(http.StatusOK)
+	err := json.NewEncoder(w).Encode(data.GetContacts())
 	if err != nil {
-		message := fmt.Sprintf("JSON marshaling error: %v", err)
+		message := fmt.Sprintf("Unable to encode data: %v", err)
 		http.Error(w, message, http.StatusInternalServerError)
 		log.Println(message)
 		return
-	}
-
-	w.Header().Add("Content-Type", "application/json; charset=utf-8")
-	w.WriteHeader(http.StatusOK)
-
-	_, err = w.Write(binaryData)
-	if err != nil {
-		message := fmt.Sprintf("Handler write error: %v", err)
-		http.Error(w, message, http.StatusInternalServerError)
-		log.Println(message)
 	}
 }
 
@@ -40,14 +34,60 @@ func AddContact(w http.ResponseWriter, r *http.Request) {
 		log.Println(message)
 		return
 	}
-	log.Printf("%+v", contact)
+	id := data.AddContact(contact)
+	w.Header().Add("Location", r.URL.String()+"/"+strconv.Itoa(id))
 	w.WriteHeader(http.StatusCreated)
 }
 
+//EditContact обрабатывает PUT запрос
 func EditContact(w http.ResponseWriter, r *http.Request) {
+	var contact data.Contact
+	err := json.NewDecoder(r.Body).Decode(&contact)
+	if err != nil {
+		message := fmt.Sprintf("Unable to decode PUT data: %v", err)
+		http.Error(w, message, http.StatusUnsupportedMediaType)
+		log.Println(message)
+		return
+	}
+	vars := mux.Vars(r)
+	id, err := strconv.Atoi(vars["id"])
+	if err != nil {
+		message := fmt.Sprintf("Incorrect ID: %v", err)
+		http.Error(w, message, http.StatusBadRequest)
+		log.Println(message)
+		return
+	}
+
+	err = data.EditContact(contact, id)
+	if err != nil {
+		message := fmt.Sprintf("Incorrect ID: %v", err)
+		http.Error(w, message, http.StatusBadRequest)
+		log.Println(message)
+		return
+	}
+	w.Header().Add("Location", r.URL.String())
+	w.WriteHeader(http.StatusAccepted)
 
 }
 
+// DeleteContact обрабатывает DELETE запрос
 func DeleteContact(w http.ResponseWriter, r *http.Request) {
+	vars := mux.Vars(r)
+	id, err := strconv.Atoi(vars["id"])
+	if err != nil {
+		message := fmt.Sprintf("Incorrect ID: %v", err)
+		http.Error(w, message, http.StatusBadRequest)
+		log.Println(message)
+		return
+	}
 
+	err = data.RemoveContact(id)
+	if err != nil {
+		message := fmt.Sprintf("Incorrect ID: %v", err)
+		http.Error(w, message, http.StatusBadRequest)
+		log.Println(message)
+		return
+	}
+	w.Header().Add("Location", r.URL.String())
+	w.WriteHeader(http.StatusNoContent)
 }
