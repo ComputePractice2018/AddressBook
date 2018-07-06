be868f0859169dd46bd8726d681a26fe59ecf976
Ivan Belyavtsev <ibelyavtsev@compute-practice.ru>
2018-07-06 20:35:16 +0300

    Добвалена работа c REST API Задача #18

--- frontend/src/components/AddressBook.vue
+++ frontend/src/components/AddressBook.vue
@@ -2,6 +2,8 @@
   <div class="addressbook">
     <h1>{{ title }}</h1>
 
+    <h3 v-if="error">Ошибка: {{error}}</h3>
+
     <table>
       <tr>
         <th>Имя</th>
@@ -29,12 +31,14 @@
       <p>Email: <input type="text" v-model="new_contact.email"></p>
       <p>Gihub: <input type="text" v-model="new_contact.github"></p>
       <button v-if="edit_index == -1" v-on:click="add_new_contact">Добавить контакт</button>
-      <button v-if="edit_index > -1" v-on:click="make_new_contact">Закончить редактирование</button>
+      <button v-if="edit_index > -1" v-on:click="end_of_edition">Закончить редактирование</button>
     </form>
   </div>
 </template>
 
 <script>
+const axios = require('axios')
+
 export default {
   name: 'AddressBook',
   props: {
@@ -43,22 +47,8 @@ export default {
   data: function () {
     return {
       edit_index: -1,
-      contact_list: [
-        {
-          'name': 'Иван',
-          'surname': 'Иванов',
-          'phone': '+7-999-999-99-99',
-          'email': 'iivanov@domain.ru',
-          'github': 'iivanov'
-        },
-        {
-          'name': 'Имя',
-          'surname': 'Фамилия',
-          'phone': '+7-999-999-99-98',
-          'email': 'user@domain.ru',
-          'github': 'user'
-        }
-      ],
+      error: '',
+      contact_list: [],
       new_contact:
         {
           'name': '',
@@ -69,26 +59,56 @@ export default {
         }
     }
   },
+  mounted: function () {
+    this.get_contacts()
+  },
   methods: {
+    get_contacts: function () {
+      this.error = ''
+      const url = '/api/addressbook/contacts'
+      axios.get(url).then(response => {
+        this.contact_list = response.data
+      }).catch(response => {
+        this.error = response.response.data
+      })
+    },
     add_new_contact: function () {
-      this.contact_list.push(this.new_contact)
+      this.error = ''
+      const url = '/api/addressbook/contacts'
+      axios.post(url, this.new_contact).then(response => {
+        this.contact_list.push(this.new_contact)
+      }).catch(response => {
+        this.error = response.response.data
+      })
     },
     remove_contact: function (item) {
-      this.contact_list.splice(this.contact_list.indexOf(item), 1)
+      this.error = ''
+      const url = '/api/addressbook/contacts/' + this.contact_list.indexOf(item)
+      axios.delete(url).then(response => {
+        this.contact_list.splice(this.contact_list.indexOf(item), 1)
+      }).catch(response => {
+        this.error = response.response.data
+      })
     },
     edit_contact: function (item) {
       this.edit_index = this.contact_list.indexOf(item)
       this.new_contact = this.contact_list[this.edit_index]
     },
-    make_new_contact: function () {
-      this.edit_index = -1
-      this.new_contact = {
-        'name': '',
-        'surname': '',
-        'phone': '',
-        'email': '',
-        'github': ''
-      }
+    end_of_edition: function () {
+      this.error = ''
+      const url = '/api/addressbook/contacts/' + this.edit_index
+      axios.put(url, this.new_contact).then(response => {
+        this.edit_index = -1
+        this.new_contact = {
+          'name': '',
+          'surname': '',
+          'phone': '',
+          'email': '',
+          'github': ''
+        }
+      }).catch(response => {
+        this.error = response.response.data
+      })
     }
   }
 }
