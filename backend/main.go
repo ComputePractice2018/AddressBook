d9b6ad16b04ca19c8eca93052c2184c442aa25c9
Ivan Belyavtsev <ibelyavtsev@compute-practice.ru>
2018-07-09 18:50:45 +0300

    Протестировать и отрефакторить сервер Задача #22

--- backend/main.go
+++ backend/main.go
@@ -4,8 +4,6 @@ import (
 	"log"
 	"net/http"
 
-	"github.com/gorilla/mux"
-
 	"github.com/ComputePractice2018/AddressBook/backend/data"
 	"github.com/ComputePractice2018/AddressBook/backend/server"
 )
@@ -14,11 +12,7 @@ func main() {
 	//name := flag.String("name", "Иван", "имя для преветствия")
 	//flag.Parse()
 	contactList := data.NewContactList()
-	router := mux.NewRouter()
-	router.HandleFunc("/api/addressbook/contacts", server.GetContacts(contactList)).Methods("GET")
-	router.HandleFunc("/api/addressbook/contacts", server.AddContact(contactList)).Methods("POST")
-	router.HandleFunc("/api/addressbook/contacts/{id}", server.EditContact(contactList)).Methods("PUT")
-	router.HandleFunc("/api/addressbook/contacts/{id}", server.DeleteContact(contactList)).Methods("DELETE")
+	router := server.NewRouter(contactList)
 
 	log.Fatal(http.ListenAndServe(":8080", router))
 }
---
+++ backend/server/handlers_test.go
@@ -0,0 +1,83 @@
+package server
+
+import (
+	"net/http"
+	"net/http/httptest"
+	"strings"
+	"testing"
+
+	"github.com/ComputePractice2018/AddressBook/backend/data"
+)
+
+var testContact = "{\"name\":\"Сергей\",\"surname\":\"Сергеев\",\"phone\":\"+7-999-999-99-99\",\"email\":\"mail@domain.ru\",\"github\":\"user\"}"
+
+func TestCrudHandlers(t *testing.T) {
+	cl := data.NewContactList()
+	router := NewRouter(cl)
+
+	req, err := http.NewRequest("GET", "/api/addressbook/contacts", nil)
+	if err != nil {
+		t.Fatal(err)
+	}
+
+	w := httptest.NewRecorder()
+	router.ServeHTTP(w, req)
+
+	resp := w.Result()
+
+	if resp.StatusCode != http.StatusOK {
+		t.Error("Expected 200 code")
+	}
+	if resp.Header.Get("Content-Type") != "application/json; charset=utf-8" {
+		t.Error("Expected json MIME-type")
+	}
+
+	testData := strings.NewReader(testContact)
+	req, err = http.NewRequest("POST", "/api/addressbook/contacts", testData)
+
+	w = httptest.NewRecorder()
+	router.ServeHTTP(w, req)
+	resp = w.Result()
+
+	if resp.StatusCode != http.StatusCreated {
+		t.Errorf("Expected 201 code (gotten: %d)", resp.StatusCode)
+	}
+	if resp.Header.Get("Location") != "/api/addressbook/contacts/0" {
+		t.Error("Expected another location")
+	}
+
+	if len(cl.GetContacts()) != 1 {
+		t.Error("Expected new value")
+	}
+
+	testData = strings.NewReader(testContact)
+	req, err = http.NewRequest("PUT", "/api/addressbook/contacts/0", testData)
+
+	w = httptest.NewRecorder()
+	router.ServeHTTP(w, req)
+	resp = w.Result()
+
+	if resp.StatusCode != http.StatusAccepted {
+		t.Errorf("Expected 201 code (gotten: %d)", resp.StatusCode)
+	}
+	if resp.Header.Get("Location") != "/api/addressbook/contacts/0" {
+		t.Error("Expected another location")
+	}
+
+	if len(cl.GetContacts()) != 1 {
+		t.Error("Expected old value")
+	}
+
+	req, err = http.NewRequest("DELETE", "/api/addressbook/contacts/0", nil)
+
+	w = httptest.NewRecorder()
+	router.ServeHTTP(w, req)
+	resp = w.Result()
+
+	if resp.StatusCode != http.StatusNoContent {
+		t.Errorf("Expected 204 code (gotten: %d)", resp.StatusCode)
+	}
+	if len(cl.GetContacts()) != 0 {
+		t.Error("Expected old value")
+	}
+}
